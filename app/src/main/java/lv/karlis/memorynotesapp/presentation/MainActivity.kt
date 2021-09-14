f88b6150c680be5154668776cb34095badf9c616
kstekels <k.stekels@gmail.com>
2021-09-14 12:12:51 +0300

    Setup project gradle

---
+++ app/src/main/java/lv/karlis/memorynotesapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package lv.karlis.memorynotesapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import lv.karlis.memorynotesapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
