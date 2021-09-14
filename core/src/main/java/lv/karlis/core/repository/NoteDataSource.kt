dc9b068ef19976f8be7718662d354b1a004c302b
kstekels <k.stekels@gmail.com>
2021-09-14 12:34:09 +0300

    Create Data Source Repository

---
+++ core/src/main/java/lv/karlis/core/repository/NoteDataSource.kt
@@ -0,0 +1,15 @@
+package lv.karlis.core.repository
+
+import lv.karlis.core.data.Note
+
+interface NoteDataSource {
+
+    suspend fun add(note: Note)
+
+    suspend fun get(id: Long): Note?
+
+    suspend fun getAll(): List<Note>
+
+    suspend fun remove(note: Note)
+
+}
